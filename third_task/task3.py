680716b09e4955daea9a2d8339d35155562825d2
juliancoffee <lightdarkdaughter@gmail.com>
2020-06-11 18:11:16 +0300

    third task

--- third_task/task3.py
+++ third_task/task3.py
@@ -1,4 +1,5 @@
-from typing import Sequence, List, TypeVar
+from typing import Sequence, List, TypeVar, Tuple
+import math
 
 N = TypeVar("N", int, float)
 
@@ -34,8 +35,32 @@ def average(xs: Sequence[N]) -> float:
         return sum(xs) / length
 
 
-def convidence_interval_dispersion():
+def convidence_interval_dispersion(
+        xs: List[N], t: float
+) -> Tuple[float, float]:
+    n = len(xs)
+    k = n - 1
+    if k <= 30:
+        raise NotImplementedError
+    x1_2 = ((math.sqrt(2*k - 1) - t) ** 2)/2
+    x2_2 = ((math.sqrt(2*k - 1) + t) ** 2)/2
+    Dx = dispersion(xs)
+    return (n*Dx / x1_2, n*Dx / x2_2)
 
 
-def convidence_interval_expected_value():
-    pass
+def convidence_interval_expected_value(
+        xs: List[N], t: float
+) -> Tuple[float, float]:
+    d = dispersion(xs)
+    sigma = math.sqrt(d)
+    x = average(xs)
+    n = len(xs)
+    k = t * sigma / math.sqrt(n)
+    return (x - k, x + k)
+
+
+if __name__ == "__main__":
+    """ За таблицею t = 1.96 ( Ф(1.96) = 0.4750 ) """
+    t = 1.96
+    print(f"Expected value = {convidence_interval_expected_value(DATA, t)}")
+    print(f"Dispersion = {convidence_interval_dispersion(DATA, t)}")
